6953e6a999afab41aec72cf60926968d91f035be
Okacz <michalwronski1@gmail.com>
2015-11-22 01:11:38 +0100

    ded

---
+++ Assets/Cope! Free Skybox Pack/LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+   
+
+    public void LoadSceneKurwo()
+    {
+        Application.LoadLevel("scena1");
+    }
+}

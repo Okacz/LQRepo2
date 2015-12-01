315832719941ccdb011fd1a9cc9a0cbf49309857
Okacz <michalwronski1@gmail.com>
2015-12-01 16:23:10 +0100

    strafing up

--- Assets/Standard Assets/Characters/FirstPersonCharacter/SimpleMovement.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/SimpleMovement.cs
@@ -32,19 +32,29 @@ public class SimpleMovement : MonoBehaviour
     {
         
         CharacterController controller = GetComponent<CharacterController>();
+
         transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.left);
+        float strafingspeed = Time.deltaTime * speed;
+        if (Input.GetKey(KeyCode.Q))
+        {
+            
+            //controller.SimpleMove(right * curSpeed);
+            // transform.Translate(new Vector3(1, 0, 0), Space.World);
+            controller.Move(-transform.right*strafingspeed);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            //controller.SimpleMove(right * curSpeed);
+            // transform.Translate(new Vector3(1, 0, 0), Space.World);
+            controller.Move(transform.right*strafingspeed);
+        }
         float cS = speed * Input.GetAxis("Vertical");
         curSpeed = cS;
         ChangeAnimation();
         controller.SimpleMove(forward * curSpeed);
-        if (Input.GetKey(KeyCode.Q))
-        {
-            //controller.SimpleMove(right * curSpeed);
-           // transform.Translate(new Vector3(1 * curSpeed, 0, 0), Space.World);
-            controller.Move(right * curSpeed/10);
-        }
+       
         if (Input.GetKey(KeyCode.Space))
         {
             if(controller.isGrounded)
